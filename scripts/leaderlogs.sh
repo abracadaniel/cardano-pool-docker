446c089a22d574029e7bda0c8947794f817f895e
Daniel <dro@arrakis.it>
2021-03-11 19:55:15 +0100

    updated leaderlogs

--- scripts/leaderlogs.sh
+++ scripts/leaderlogs.sh
@@ -7,20 +7,8 @@ POOL_ID=$(cat ${NODE_PATH}/staking/POOL_ID)
 TZ=$(cat /etc/timezone)
 VRF=${NODE_PATH}/staking/pool-keys/vrf.skey
 
-echo "Dumping ledger.json"
-cardano-cli query ledger-state ${ERA_ARGUMENT} ${NETWORK_ARGUMENT} --out-file ${NODE_PATH}/ledger.json
-
-echo "Calculating sigma"
-SIGMA=$(python3 /scripts/pooltool.io/leaderLogs/getSigma.py --pool-id ${POOL_ID} --ledger ${NODE_PATH}/ledger.json | tail -1 | awk '{print $2}')
-
-# Print vars
-echo "POOL_ID: ${SIGMA}"
-echo "TZ: ${TZ}"
-echo "VRF: ${VRF}"
-echo "SIGMA: ${SIGMA}"
-
 # Get leaderlogs
 python3 /scripts/pooltool.io/leaderLogs/leaderLogs.py \
     --vrf-skey ${VRF} \
-    --sigma ${SIGMA} \
+    --pool-id ${POOL_ID} \
     --tz ${TZ}
