47449fafd2960adeb4d6b2ef057b5ea3bccf533d
abracadaniel <droe@pm.me>
2024-10-31 12:34:31 +0100

    updated versions. Node to 10.1.1.

--- build.sh
+++ build.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
-MITHRIL_VERSION="2437.1"
-NODE_VERSION="9.2.1"
+MITHRIL_VERSION="2442.0"
+NODE_VERSION="10.1.1"
 LIBSODIUM_VERSION="dbb48cc"
 ADDRESSES_VERSION="3.12.0"
-BECH_VERSION="1.1.5"
-CNCLI_VERSION="6.3.0"
-POOL_VERSION="9.2.1"
+BECH_VERSION="1.1.7"
+CNCLI_VERSION="6.5.0"
+POOL_VERSION="10.1.1"
 
-docker build --network host --output type=docker -f Dockerfile.node \
+docker build --output type=docker -f Dockerfile.node \
     --build-arg VERSION=${NODE_VERSION} \
     --tag arradev/cardano-node:${NODE_VERSION} \
     --tag arradev/cardano-node:latest .
 
-docker build --network host --output type=docker -f Dockerfile.mithril-client \
+docker build --output type=docker -f Dockerfile.mithril-client \
     --build-arg VERSION=${MITHRIL_VERSION} \
     --tag arradev/mithril-client:${MITHRIL_VERSION} \
     --tag arradev/mithril-client:latest .
 
-docker build --network host --output type=docker -f Dockerfile.mithril-signer \
+docker build --output type=docker -f Dockerfile.mithril-signer \
     --build-arg VERSION=${MITHRIL_VERSION} \
     --tag arradev/mithril-signer:${MITHRIL_VERSION} \
     --tag arradev/mithril-signer:latest .
 
-docker build --network host --output type=docker -f Dockerfile.addresses \
+docker build --output type=docker -f Dockerfile.addresses \
     --build-arg VERSION=${ADDRESSES_VERSION} \
     --tag arradev/cardano-addresses:${ADDRESSES_VERSION} \
     --tag arradev/cardano-addresses:latest .
 
-docker build --network host --output type=docker -f Dockerfile.bech32 \
+docker build --output type=docker -f Dockerfile.bech32 \
     --build-arg VERSION=${BECH_VERSION} \
     --tag arradev/bech32:${BECH_VERSION} \
     --tag arradev/bech32:latest .
 
-docker build --network host --output type=docker -f Dockerfile.cncli \
+docker build --output type=docker -f Dockerfile.cncli \
     --build-arg VERSION=${CNCLI_VERSION} \
     --tag arradev/cncli:${CNCLI_VERSION} \
     --tag arradev/cncli:latest .
 
-docker build --network host --output type=docker -f Dockerfile.pool \
+docker build --output type=docker -f Dockerfile.pool \
     --build-arg NODE_VERSION=${NODE_VERSION} \
     --tag arradev/cardano-pool:${POOL_VERSION} \
     --tag arradev/cardano-pool:latest .
