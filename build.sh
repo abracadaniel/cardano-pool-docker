f9e56dae03246bc4233b406f93db2ee54d723a63
Robin Böning <robin.boening@gmail.com>
2025-08-22 22:58:40 +0200

    make docker user variable with fallback
    This change allows to build the whole stack locally without writing the images
    out as arradev/xyz
    
    With arradev as default, the behaviour stays as is, unless the build
    command is prompted like `DOCKER_USER=foobar ./build.sh`.

--- build.sh
+++ build.sh
@@ -7,38 +7,42 @@ ADDRESSES_VERSION="4.0.0"
 BECH_VERSION="1.1.7"
 CNCLI_VERSION="6.5.1"
 POOL_VERSION="10.4.1-2"
+DOCKER_USER="${DOCKER_USER:-arradev}"
 
 docker build -f Dockerfile.node \
     --build-arg VERSION=${NODE_VERSION} \
-    --tag arradev/cardano-node:${NODE_VERSION} \
-    --tag arradev/cardano-node:latest .
+    --tag ${DOCKER_USER}/cardano-node:${NODE_VERSION} \
+    --tag ${DOCKER_USER}/cardano-node:latest .
 
 docker build -f Dockerfile.mithril-client \
     --build-arg VERSION=${MITHRIL_VERSION} \
-    --tag arradev/mithril-client:${MITHRIL_VERSION} \
-    --tag arradev/mithril-client:latest .
+    --tag ${DOCKER_USER}/mithril-client:${MITHRIL_VERSION} \
+    --tag ${DOCKER_USER}/mithril-client:latest .
 
 docker build -f Dockerfile.mithril-signer \
     --build-arg VERSION=${MITHRIL_VERSION} \
-    --tag arradev/mithril-signer:${MITHRIL_VERSION} \
-    --tag arradev/mithril-signer:latest .
+    --build-arg DOCKER_USER=${DOCKER_USER} \
+    --tag ${DOCKER_USER}/mithril-signer:${MITHRIL_VERSION} \
+    --tag ${DOCKER_USER}/mithril-signer:latest .
 
 docker build -f Dockerfile.addresses \
     --build-arg VERSION=${ADDRESSES_VERSION} \
-    --tag arradev/cardano-addresses:${ADDRESSES_VERSION} \
-    --tag arradev/cardano-addresses:latest .
+    --tag ${DOCKER_USER}/cardano-addresses:${ADDRESSES_VERSION} \
+    --tag ${DOCKER_USER}/cardano-addresses:latest .
 
 docker build -f Dockerfile.bech32 \
     --build-arg VERSION=${BECH_VERSION} \
-    --tag arradev/bech32:${BECH_VERSION} \
-    --tag arradev/bech32:latest .
+    --tag ${DOCKER_USER}/bech32:${BECH_VERSION} \
+    --tag ${DOCKER_USER}/bech32:latest .
 
 docker build -f Dockerfile.cncli \
     --build-arg VERSION=${CNCLI_VERSION} \
-    --tag arradev/cncli:${CNCLI_VERSION} \
-    --tag arradev/cncli:latest .
+    --build-arg DOCKER_USER=${DOCKER_USER} \
+    --tag ${DOCKER_USER}/cncli:${CNCLI_VERSION} \
+    --tag ${DOCKER_USER}/cncli:latest .
 
 docker build -f Dockerfile.pool \
     --build-arg NODE_VERSION=${NODE_VERSION} \
-    --tag arradev/cardano-pool:${POOL_VERSION} \
-    --tag arradev/cardano-pool:latest .
+    --build-arg DOCKER_USER=${DOCKER_USER} \
+    --tag ${DOCKER_USER}/cardano-pool:${POOL_VERSION} \
+    --tag ${DOCKER_USER}/cardano-pool:latest .
